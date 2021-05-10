2d75fc67211e4809a568dca7a1be005bc03afc09
Rishabh5695 <76728871+Rishabh5695@users.noreply.github.com>
2021-05-10 16:11:11 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,48 @@
+const Engine=Matter.Engine
+const World=Matter.World
+const Bodies=Matter.Bodies
+var bg1,bg2,bg3
+var maxsnow
+var snow=[]
+
+function preload(){
+  bg1=loadImage("snow1.jpg")
+  bg2=loadImage("snow2.jpg")
+  bg3=loadImage("snow3.jpg")
+
+}
+function setup() {
+  createCanvas(800,400);
+  engine=Engine.create()
+  world=engine.world
+}
+
+function draw() {
+  var rand=Math.round(random(1,3))
+  if(frameCount%200===0){
+
+  if(rand===1){
+  background(bg1);  
+    
+  }
+ else if(rand===2){
+    background(bg2);  
+      
+    }
+ else if(rand===3){
+      background(bg3);  
+        
+      }
+      else{
+        background(bg1);  
+  
+
+      }
+  }
+if(frameCount%5===0){
+  snow.push(new Snow(random(0,800),30,30))
+}
+for(var j=0;j<snow.length;j++){
+  snow[j].display();
+}
+}
